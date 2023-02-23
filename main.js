16a19ccb180d9210d7c7ea90782e4d6d05f31838
atuls9 <atul.sooryawanshi@gmail.com>
2023-02-23 20:17:24 +0530

    localstorage DELETE button DOM task 13 done

--- main.js
+++ main.js
@@ -1,60 +1,35 @@
-// DOM Elements
-const studentForm = document.getElementById("studentForm");
-const studentsContainer = document.querySelector(".students");
-const nameInput = studentForm["name"];
-const emailInput = studentForm["email"];
-const pnumberInput = studentForm["pnumber"];
 
-
-const students = JSON.parse(localStorage.getItem("students")) || [];
-
-const addStudent = (name, email, pnumber) => {
-    students.push({
+function getInput(event){
+    event.preventDefault();
+    var name = event.target.username.value;
+    var email = event.target.email.value;
+    var pnumber = event.target.pnumber.value;
+        // alert(name)
+    var obj = {
         name,
         email,
-        pnumber,
-    });
+        pnumber
+    }
+    localStorage.setItem(obj.email, JSON.stringify(obj));
+    showOnScreen(obj)
+    
+}
 
-    localStorage.setItem(emailInput.value, JSON.stringify(students));
+    function showOnScreen(obj){
+        // e.preventDefault();
+        var parentel = document.getElementById('listofitems');
+        var child = document.createElement('li');
+        child.textContent = obj.name + "-" + obj.email + '-' + obj.pnumber;
+       
+        var deleteButton = document.createElement('input');
+        deleteButton.type = 'button';
+        deleteButton.value = "DELETE";
+        deleteButton.onclick = () =>{
+            localStorage.removeItem(obj.email);
+            parentel.removeChild(child);
+        }
+        
+        child.appendChild(deleteButton);
+        parentel.appendChild(child);
 
-    return { name, email, pnumber };
-};
-
-const createStudentElement = ({ name, email, pnumber }) => {
-    // Create elements
-    const studentDiv = document.createElement("div");
-    const studentName = document.createElement("h4");
-    const studentEmail = document.createElement("h4");
-    const studentPnumber = document.createElement("h4");
-
-    // Fill the content
-    studentName.innerText = "Student Name: " + name;
-    studentEmail.innerText = "Student Email: " + email;
-    studentPnumber.innerText = "Student Phone Number: " + pnumber;
-
-    // Add to the DOM
-    studentDiv.append(studentName, studentEmail, studentPnumber);
-    studentsContainer.appendChild(studentDiv);
-
-    studentsContainer.style.display = students.length === 0 ? "none" : "flex";
-};
-
-studentsContainer.style.display = students.length === 0 ? "none" : "flex";
-
-students.forEach(createStudentElement);
-
-studentForm.onsubmit = e => {
-    e.preventDefault();
-
-    const newStudent = addStudent(
-        nameInput.value,
-        emailInput.value,
-        pnumberInput.value
-    );
-
-    createStudentElement(newStudent);
-
-    nameInput.value = "";
-    emailInput.value = "";
-    pnumberInput.value = "";
-};
+    }
